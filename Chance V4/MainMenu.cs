f849ac17d47d1fb66ed001228f9b479f57c2e5ea
XxJOxX25 <johnoscarbhv@gmail.com>
2022-06-14 19:03:52 +0200

    Version 1.1 - release

---
+++ Chance V4/MainMenu.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Chance_V4
+{
+    internal class MainMenu
+    {
+
+        public void LineCreation()
+        {
+            int current = 0;
+
+            while (current <= 58)
+            {
+                Thread.Sleep(10);
+
+                Console.Write("-");
+                current = current + 1;
+            }
+        }
+
+        public void Menu()
+        {
+            Console.Clear();
+            Thread.Sleep(1000);
+
+            string lineWrite = @"
+  _____  _                               __      __ _  _   
+ / ____|| |                              \ \    / /| || |  
+| |     | |__    __ _  _ __    ___  ___   \ \  / / | || |_ 
+| |     | '_ \  / _` || '_ \  / __|/ _ \   \ \/ /  |__   _|
+| |____ | | | || (_| || | | || (__|  __/    \  /      | |  
+ \_____||_| |_| \__,_||_| |_| \___|\___|     \/       |_|  
+                                                           
+                                                           
+";
+
+            Thread.Sleep(500);
+            Console.WriteLine(lineWrite);
+
+            Thread.Sleep(500);
+            Console.WriteLine("Credits:");
+
+            Console.WriteLine("");
+
+            Thread.Sleep(500);
+
+            Console.WriteLine("By Jo");
+            Thread.Sleep(250);
+            Console.WriteLine("https://github.com/JoDotNet");
+
+            LineCreation();
+        }
+    }
+}
--- Chance V4/Program.cs
+++ Chance V4/Program.cs
@@ -13,9 +13,35 @@ namespace Chance_V4
         {
             // Classes
             Functions functions = new Functions();
+            MainMenu mainMenu = new MainMenu();
 
-            
+            mainMenu.Menu();
 
+            Thread.Sleep(2500);
+
+            functions.AskUser();
+            functions.GeneratingWait();
+            functions.Chance();
+
+            Thread.Sleep(750);
+
+            functions.Result();
+
+            Console.WriteLine("");
+            Console.WriteLine("Press ENTER to close . . .");
+            Console.ReadLine();
+
+
+
+
+            /*
+             * ADD: Main Menu
+             * ADD: Percentage Calculator
+             * ADD: Probability Calculator P(x, x) = 1/6 * 13/52 = 13/312 = 0.24 = 24%
+             */
+
+
+            /*
             // Main
             functions.AskUser();
 
@@ -30,6 +56,7 @@ namespace Chance_V4
             Console.WriteLine("");
             Console.WriteLine("Press ENTER to close . . .");
             Console.ReadLine();
+            */
 
         }
     }
